24fe85a79ff99cd629ec84137cf4756ca3b18b35
MKM2626 <mkmiller06@icloud.com>
2026-08-21 21:50:21 +0800

    Allowed one to change if the fine tune is finalised

--- src/routes/edit/[id]/+page.svelte
+++ src/routes/edit/[id]/+page.svelte
@@ -2,6 +2,7 @@
     import { getFineTune, getAnalysts, editForm, search } from "#lib/remote/registers.remote";
 	import { getSearchContext } from "#lib/context/search";
     import { goto } from "$app/navigation";
+	import { boolean } from "drizzle-orm/pg-core/columns";
 
     const searchInfo = getSearchContext()
 
@@ -12,6 +13,7 @@
     let fineTune = $derived(await getFineTune(params.id));
 
     let analysts = await getAnalysts();
+    let finalised = $state(false)
 </script>
 
 
@@ -40,10 +42,7 @@
         </div>
     </div>
 
-    <input 
-        type="hidden"
-        {...editForm.fields.id.as("select", id)}
-    />
+    <input type="hidden" {...editForm.fields.id.as("select", id)}/>
 
     <div class="bg-bg-light border border-border rounded-lg p-5 flex flex-col gap-3">
         <div class="flex">
@@ -88,21 +87,55 @@
                 {fineTune.technology}
             </span>
         </div>
+
+        {#if fineTune.finalised} 
+            <div class="flex">
+                <span class="w-32 text-text-muted">
+                    Finalised: 
+                </span>
+                <input type="checkbox" checked={fineTune.finalised} disabled 
+                    class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom -bottom-0.75 relative flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
+                        ${fineTune.finalised ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
+                >
+            </div>
+        {/if}
+
+        <div class="flex">
+            <span class="w-32 text-text-muted">
+                Global: 
+            </span>
+            <input type="checkbox" checked={fineTune.global} disabled class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom relative -bottom-0.75 flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
+                ${fineTune.global ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
+            >
+        </div>
     </div>
+
+    {#if !fineTune.finalised}
+        <div class="mt-5">
+            <h3 class="text-xl mb-2">
+                Finalised:
+            </h3>
+
+            <div class="bg-bg-light border border-border rounded-lg p-4 flex items-center gap-3">
+            
+                <input
+                    {...editForm.fields.finalised.as("checkbox", false)}
+                    class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom relative flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
+                        ${editForm.fields.finalised.value() ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
+                />  
+                <label for='finalise'>
+                   Once finalised, you won't be able to edit the fine tune.
+                </label>
+            </div>
+        </div>
+    {/if}
 
     <div class="mt-5">
         <h3 class="text-xl mb-2">
             Before:
         </h3>
 
-        <div
-            class="
-                bg-bg-light
-                border border-border
-                rounded-lg
-                p-4
-            "
-        >
+        <div class="bg-bg-light border border-border rounded-lg p-4">
             {fineTune.before}
         </div>
     </div>
@@ -116,10 +149,7 @@
            <select
                 id="analyst"
                 class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
-                {...editForm.fields.analystId.as(
-                    "select",
-                    fineTune.analystId!
-                )}
+                {...editForm.fields.analystId.as("select", fineTune.analystId!)}
             >
                 {#each analysts as analyst}
 
@@ -138,12 +168,16 @@
         </h3>
 
         <div class="bg-bg-light border border-border rounded-lg p-4">
-           <textarea
-                id="after"
-                rows="4"
-                class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
-                {...editForm.fields.after.as("text",fineTune.after)}
-            ></textarea>
+            {#if fineTune.finalised}
+                {fineTune.after}
+            {:else}
+                <textarea
+                    id="after"
+                    rows="4"
+                    class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
+                    {...editForm.fields.after.as("text",fineTune.after)}
+                ></textarea>
+            {/if}
         </div>
     </div>
 
