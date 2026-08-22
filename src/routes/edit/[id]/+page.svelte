8cc05c6b0bf596810ffd69e3ece1fce4abdecba9
MKM2626 <mkmiller06@icloud.com>
2026-08-22 16:18:33 +0800

    Used effect to set initial edit form field, so fields change when changine selected fine tune

--- src/routes/edit/[id]/+page.svelte
+++ src/routes/edit/[id]/+page.svelte
@@ -2,18 +2,30 @@
     import { getFineTune, getAnalysts, editForm, search } from "#lib/remote/registers.remote";
 	import { getSearchContext } from "#lib/context/search";
     import { goto } from "$app/navigation";
-	import { boolean } from "drizzle-orm/pg-core/columns";
 
     const searchInfo = getSearchContext()
 
     let { params } = $props();
 
-    let id = $derived(params.id)
-
     let fineTune = $derived(await getFineTune(params.id));
 
     let analysts = await getAnalysts();
-    let finalised = $state(false)
+
+    let lastId = ''
+
+    $effect(() => {
+        const id = params.id
+
+        if (params.id == lastId) return
+
+        lastId = id
+
+        editForm.fields.id.set(params.id)
+        editForm.fields.after.set(fineTune.after);
+		editForm.fields.comment.set(fineTune.comment ?? "");
+		editForm.fields.analystId.set(fineTune.analystId);
+		editForm.fields.finalised.set(fineTune.finalised);
+    })
 </script>
 
 
@@ -26,7 +38,7 @@
         <div class="flex gap-3">
             <button
                 type="button"
-                onclick={()=>goto(`/details/${id}`)}
+                onclick={()=>goto(`/details/${params.id}`)}
                 class="px-4 py-2 rounded-lg bg-bg-light border border-border hover:bg-linear-to-b hover:from-gradient-start hover:to-gradient-end transition"
             >
                 Cancel
@@ -42,7 +54,7 @@
         </div>
     </div>
 
-    <input type="hidden" {...editForm.fields.id.as("select", id)}/>
+    <input type="hidden" {...editForm.fields.id.as("select")}/>
 
     <div class="bg-bg-light border border-border rounded-lg p-5 flex flex-col gap-3">
         <div class="flex">
@@ -93,10 +105,7 @@
                 <span class="w-32 text-text-muted">
                     Finalised: 
                 </span>
-                <input type="checkbox" checked={fineTune.finalised} disabled 
-                    class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom -bottom-0.75 relative flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
-                        ${fineTune.finalised ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
-                >
+                <input type="checkbox" checked={fineTune.finalised} disabled class="appearance-none h-4 w-4 rounded-xs shadow-sm border border-red-500 bg-red-500 flex items-center justify-center cursor-pointer before:content-['✗'] before:text-white before:text-xs before:font-medium checked:bg-green-500 checked:border-green-500 checked:before:content-['✓']">
             </div>
         {/if}
 
@@ -104,9 +113,7 @@
             <span class="w-32 text-text-muted">
                 Global: 
             </span>
-            <input type="checkbox" checked={fineTune.global} disabled class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom relative -bottom-0.75 flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
-                ${fineTune.global ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
-            >
+            <input type="checkbox" checked={fineTune.global} disabled class="appearance-none h-4 w-4 rounded-xs shadow-sm border border-red-500 bg-red-500 flex items-center justify-center cursor-pointer before:content-['✗'] before:text-white before:text-xs before:font-medium checked:bg-green-500 checked:border-green-500 checked:before:content-['✓']">
         </div>
     </div>
 
@@ -118,13 +125,9 @@
 
             <div class="bg-bg-light border border-border rounded-lg p-4 flex items-center gap-3">
             
-                <input
-                    {...editForm.fields.finalised.as("checkbox", false)}
-                    class={`appearance-none h-3 w-3 border rounded-xs shadow-sm align-bottom relative flex items-center justify-center font-bold text-[12px] text-white cursor-pointer
-                        ${editForm.fields.finalised.value() ? 'bg-green-500 border-green-500 before:content-["✓"]' : 'bg-red-500 border-red-500 before:content-["✗"]'}`}
-                />  
-                <label for='finalise'>
-                   Once finalised, you won't be able to edit the fine tune.
+                <input {...editForm.fields.finalised.as("checkbox")} class="appearance-none h-4 w-4 rounded-xs shadow-sm border border-red-500 bg-red-500 flex items-center justify-center cursor-pointer before:content-['✗'] before:text-white before:text-xs before:font-medium checked:bg-green-500 checked:border-green-500 checked:before:content-['✓']"/>  
+                <label for='finalised'>
+                    Once finalised, you won't be able to edit the fine tune.
                 </label>
             </div>
         </div>
@@ -146,10 +149,10 @@
         </h3>
 
         <div class="bg-bg-light border border-border rounded-lg p-4">
-           <select
+        <select
                 id="analyst"
                 class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
-                {...editForm.fields.analystId.as("select", fineTune.analystId!)}
+                {...editForm.fields.analystId.as("select")}
             >
                 {#each analysts as analyst}
 
@@ -175,7 +178,7 @@
                     id="after"
                     rows="4"
                     class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
-                    {...editForm.fields.after.as("text",fineTune.after)}
+                    {...editForm.fields.after.as("text")}
                 ></textarea>
             {/if}
         </div>
@@ -187,12 +190,13 @@
         </h3>
 
         <div class="bg-bg-light border border-border rounded-lg p-4">
-           <textarea
+        <textarea
                 id="comment"
                 rows="4"
                 class="w-full px-3 py-2 rounded-lg bg-bg border border-border outline-none"
-                {...editForm.fields.comment.as("text",fineTune.comment!)}
+                {...editForm.fields.comment.as("text")}
             ></textarea>
         </div>
     </div>
 </form>
+
