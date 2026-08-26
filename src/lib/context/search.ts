ee0d3fd5a75299b336a6a39ac2da20736b4498db
MKM2626 <mkmiller06@icloud.com>
2026-08-26 10:46:19 +0800

    added finalise to search parameter

--- src/lib/context/search.ts
+++ src/lib/context/search.ts
@@ -1,25 +1,13 @@
 import { createContext } from "svelte";
 import { type } from 'arktype'
 
-// const SearchInfo = type({
-//     "search?": "string", 
-//      page: "number", 
-//     pageSize: "number", 
-//     descending: "boolean",
-//     "start?": "Date",
-//     "end?": "Date"
-// })
-
-// export type SearchInfo = typeof SearchInfo.infer
-
 export interface SearchInfo { 
-        search?: string, 
+        search?: string[], 
         page: number, 
         pageSize: number, 
         descending: boolean,
         start?: Date,
         end?: Date
     }
-
 
 export const [getSearchContext, setSearchContext] = createContext<SearchInfo>();
