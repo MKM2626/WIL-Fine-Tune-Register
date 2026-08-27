45f2c4ab43cc80a7d245b897d3741f1889c54808
MKM2626 <mkmiller06@icloud.com>
2026-08-27 09:52:40 +0800

    added more search functionality and export csv fetch data logic

--- src/lib/remote/registers.remote.ts
+++ src/lib/remote/registers.remote.ts
@@ -4,6 +4,7 @@ import { type } from "arktype"
 import { db } from '#lib/db'
 import { analysts, customers, fine_tunes, technologies, rules} from "#lib/db/schema";
 import { eq, and, gt, asc, desc, like, notLike, lt, count, or, gte, lte, sql, ne } from 'drizzle-orm'
+import { boolean } from "drizzle-orm/cockroach-core/columns/bool";
 
 
 const ftSchema = type("string.numeric.parse")
@@ -281,12 +282,16 @@ export const search = query(searchSchema,
         const searchTerms = data.search?.flatMap(term => term.trim().split(/\s+/)).filter(Boolean) ?? []
 
         const where = and(searchTerms.length
-            ? and(
+            ? or(
                 ...searchTerms.map(term =>
                     or(
+                        // lots or redundant searches. Some searches may not be applied, but thats how it goes.
                         like(rules.name, `%${term}%`),
                         like(customers.name, `%${term}%`),
-                        like(technologies.name, `%${term}%`)
+                        like(technologies.name, `%${term}%`),
+                        like(analysts.name, `%${term}%`),
+                        like(fine_tunes.fineTune, `%${term}%`),
+                        like(fine_tunes.comment, `%${term}%`),
                     )
                 )
             )
@@ -337,5 +342,90 @@ export const search = query(searchSchema,
             totalRows,
             totalPages
         };
+    }
+)
+
+
+export const getCSV = query(
+    searchSchema,
+    async (data) => {
+
+        const searchTerms = data.search?.flatMap(term => term.trim().split(/\s+/)).filter(Boolean) ?? []
+
+        const where = and(searchTerms.length
+            ? or(
+                ...searchTerms.map(term =>
+                    or(
+                        like(rules.name, `%${term}%`),
+                        like(customers.name, `%${term}%`),
+                        like(technologies.name, `%${term}%`),
+                        like(analysts.name, `%${term}%`),
+                        like(fine_tunes.fineTune, `%${term}%`),
+                        like(fine_tunes.comment, `%${term}%`),
+                    )
+                )
+            )
+            : undefined, 
+
+            data.start ? gte(fine_tunes.date, data.start) : undefined,
+
+            data.end ? lte(fine_tunes.date, data.end) : undefined,
+
+            data.finalised !== undefined ? eq(fine_tunes.finalised, data.finalised) : undefined
+        )
+
+        const sort = data.descending ? desc(fine_tunes.date) : fine_tunes.date
+
+        const rows = await db.select({
+            Date: fine_tunes.date, 
+            Rule: rules.name, 
+            Customer: customers.name, 
+            Technology: technologies.name,
+            "Global Id": fine_tunes.globalId,
+            "Fine Tune": fine_tunes.fineTune,
+            Finalised: sql`${fine_tunes.finalised}`.mapWith((val) => val ? "Yes" : "No"),
+            Analyst: analysts.name,
+            Comment: fine_tunes.comment
+        })
+        .from(fine_tunes)
+        .leftJoin(rules, eq(fine_tunes.ruleId, rules.id))
+        .leftJoin(customers, eq(fine_tunes.customerId, customers.id))
+        .leftJoin(technologies, eq(customers.technologyId, technologies.id))
+        .leftJoin(analysts, eq(fine_tunes.analystId, analysts.id))
+        .where(where)
+        .orderBy(sort)
+
+        let success = rows.length > 0
+        let message =  success ? undefined : "There are no records"
+
+        if (!success) {
+            return {
+                success: success,
+                message: message
+            }
+        }
+
+        let date = [data.start, data.end].filter(Boolean).join(" / ")
+
+        const distinctCustomers = await db.selectDistinct({customer: customers.name}).from(fine_tunes)
+            .leftJoin(rules, eq(fine_tunes.ruleId, rules.id))
+            .leftJoin(customers, eq(fine_tunes.customerId, customers.id))
+            .leftJoin(technologies, eq(customers.technologyId, technologies.id))
+            .leftJoin(analysts, eq(fine_tunes.analystId, analysts.id))
+            .where(where)
+        const distinctRules = await db.selectDistinct({rules: rules.name}).from(fine_tunes)
+            .leftJoin(rules, eq(fine_tunes.ruleId, rules.id))
+            .leftJoin(customers, eq(fine_tunes.customerId, customers.id))
+            .leftJoin(technologies, eq(customers.technologyId, technologies.id))
+            .leftJoin(analysts, eq(fine_tunes.analystId, analysts.id))
+            .where(where)
+
+        return {
+            success: success,
+            rows: rows,
+            distinctCustomers: distinctCustomers,
+            distinctRules: distinctRules,
+            date: date,
+        }
     }
 )
