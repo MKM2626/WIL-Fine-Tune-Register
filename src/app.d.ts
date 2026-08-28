405214a5fa2d2e8fd7739b43a3de1c612795f454
MKM2626 <mkmiller06@icloud.com>
2026-08-28 12:17:35 +0800

    added locals interface for user and sessions for better auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import type { User, Session } from 'better-auth';
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: User | null,
+			session: Session | null
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
