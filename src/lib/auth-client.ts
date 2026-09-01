6398daa87007e863e79c72794877f23725ca4b36
MKM2626 <mkmiller06@icloud.com>
2026-09-01 14:46:21 +0800

    moved  location

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,13 @@
-import { createAuthClient } from 'better-auth/client'
-const authClient = createAuthClient({})
+import { createAuthClient } from 'better-auth/svelte'
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from '#lib/server/auth'
+
+export const authClient = createAuthClient({
+    plugins: [inferAdditionalFields<typeof auth>()],
+})
 
 const signIn = async () => {
     const data = await authClient.signIn.social({
         provider: 'github'
     })
-    
 }
