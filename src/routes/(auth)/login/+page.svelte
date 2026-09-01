3693d4d4f09de025252e7cf7eeefbedeea3712a1
MKM2626 <mkmiller06@icloud.com>
2026-09-01 14:49:29 +0800

    added a base login page

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { authClient } from "#lib/auth-client";
+
+    async function login() {
+        await authClient.signIn.social({
+            provider: "github",
+            callbackURL: "/"
+        });
+    }
+
+</script>
+
+<div>
+    <h1>Login</h1>
+    <button type="button" onclick={login}>
+        Continue with GitHub
+    </button>
+
+    <button onclick={async () => {
+        await authClient.signOut({callbackURL: "http://localhost:5173"})
+    }}>
+    logout
+    </button>
+</div>
