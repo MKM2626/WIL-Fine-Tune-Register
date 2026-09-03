2087f559a5a9fb63819644e09718cdb57e86a786
MKM2626 <mkmiller06@icloud.com>
2026-09-03 10:50:40 +0800

    Implementing route guarding in hooks instead of in +page.server.ts or +layout.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,42 @@
 import { auth } from "#lib/server/auth";
-import type { Handle } from '@sveltejs/kit'
+import { type Handle, redirect } from '@sveltejs/kit'
 import { svelteKitHandler } from 'better-auth/svelte-kit'
-import { building } from "$app/env"; // Unsure if it should be set to enviroment
+import { building } from "$app/env"; 
+import { sequence } from "@sveltejs/kit/hooks";
+import { error } from '@sveltejs/kit'
 
-export const handle: Handle = async ({event, resolve}) => {
+const authentication: Handle = async ({ event, resolve }) => {
+    return svelteKitHandler({event, resolve, auth, building });
+};
+
+const session: Handle = async ({ event, resolve }) => {
     const session = await auth.api.getSession({
-        headers: event.request.headers,
-    })
+        headers: event.request.headers
+    });
 
     if (session) {
         event.locals.session = session.session;
         event.locals.user = session.user;
-        console.log(event.locals.session.token)
     }
 
-    return svelteKitHandler({event, resolve, auth, building})
-}
+    return resolve(event);
+};
+
+const route_guard: Handle = async ({ event, resolve }) => {
+    if (event.route.id?.startsWith('/(protected)') && !event.locals.session) {
+        redirect(303, 'login')
+    }
+
+    if (event.locals.user?.teams.length === 0 && event.locals.session) {
+        error(401, "You are not permitted to access this service")
+    }
+
+    if (event.route.id?.startsWith('/(protected)/create') && !event.locals.user?.teams.includes('admin') && !event.locals.user?.teams.includes('senior')) {
+        error(403, 'You are not permitted to create a new rule');
+    }
+
+    return resolve(event);
+};
+
+export const handle = sequence(authentication, session, route_guard);
 
