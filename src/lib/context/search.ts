7888adf5797cca5f5d01eb6f4222b22a0c241d50
MKM2626 <mkmiller06@icloud.com>
2026-09-04 16:11:03 +0800

    Nothing changed

--- src/lib/context/search.ts
+++ src/lib/context/search.ts
@@ -1,8 +1,5 @@
 import { createContext } from "svelte";
-import { type } from 'arktype'
 
-
-// Should use arktype
 export interface SearchInfo { 
         search?: string[], 
         page: number, 
