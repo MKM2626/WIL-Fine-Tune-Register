59df0effaddf632e7b3ad3e8b7ff7e745c5f8c3c
MKM2626 <mkmiller06@icloud.com>
2026-09-07 11:40:36 +0800

    change env to look better

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,13 @@
 import {defineEnvVars} from '@sveltejs/kit/env'
 
 export const variables = defineEnvVars({
-    GITHUB_CLIENT_ID: {},
-    GITHUB_CLIENT_SECRET: {},
-
     DB_FILE_NAME: {},
 
     BETTER_AUTH_SECRET: {},
     BETTER_AUTH_BASE_URL: {},
+
+    GITHUB_CLIENT_ID: {},
+    GITHUB_CLIENT_SECRET: {},
 
     BETTER_AUTH_TRUSTED_ORIGINS: {},
 
