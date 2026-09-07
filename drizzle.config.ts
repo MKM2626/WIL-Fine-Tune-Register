38f5081eee33d460dc0ca1f32376e5e383de54a1
MKM2626 <mkmiller06@icloud.com>
2026-09-07 11:40:13 +0800

    changing drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,25 @@
-import { Config, defineConfig } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
+// import { Config, defineConfig } from 'drizzle-kit';
+// import * as dotenv from 'dotenv';
 
-dotenv.config()
+// dotenv.config()
+
+// export default defineConfig({
+// 	out: './drizzle',
+// 	schema: './src/lib/server/db/schema.ts',
+// 	dialect: 'sqlite',
+// 	dbCredentials: {
+// 		url: "file:local.db",
+// 	}
+// }) satisfies Config;
+
+import 'dotenv/config';
+import { Config, defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-	out: './drizzle',
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: {
-		url: "file:local.db",
-	}
+  out: './drizzle',
+  schema: './src/lib/server/db/schema.ts',
+  dialect: 'sqlite',
+  dbCredentials: {
+    url: "./local.db",
+  },
 }) satisfies Config;
