559e45401727838645f613c1b31be4b292858772
MKM2626 <mkmiller06@icloud.com>
2026-09-07 11:40:45 +0800

    change index.ts

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,8 +5,8 @@
 // import { url } from 'arktype/internal/keywords/string.ts';
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql';
-import { DB_FILE_NAME } from '$app/env/private'
+// import { DB_FILE_NAME } from '$app/env/private'
 
-const client = createClient({ url: DB_FILE_NAME })
+const client = createClient({ url: './local.db' })
 
 export const db = drizzle({client})
