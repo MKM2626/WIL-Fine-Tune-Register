0133d293f1c049d083bbcd8fe3a015f649468dcc
MKM2626 <mkmiller06@icloud.com>
2026-09-12 11:51:18 +0800

    Changed seeder to match schema, haven't updated it fully

--- scripts/seeder.ts
+++ scripts/seeder.ts
@@ -35,25 +35,38 @@ const actualCustomers = await db.insert(schema.customers).values(mockCustomers).
 const mockAnalysts: (typeof schema.analysts.$inferInsert) []= [];
 for (let i = 0; i <20; i++) {
     mockAnalysts.push({
-        name: faker.person.fullName()
+        userName: faker.internet.username(),
+        name: faker.helpers.maybe(() => faker.person.fullName(), {probability: 0.7})
     })
 }
 
 const actualAnalysts = await db.insert(schema.analysts).values(mockAnalysts).returning()
 
+const mockCustomerRules: (typeof schema.customer_rules.$inferInsert) []=[]
+for (let i = 0; i <1000; i++) {
+    mockCustomerRules.push({
+        date: faker.date.between({from: new Date('2000-01-01'), to: new Date('2025-12-31T23:59:59.999Z')}),
+        ruleId: faker.helpers.arrayElement(actualRules).id,
+        customerId: faker.helpers.arrayElement(actualCustomers).id
+    })
+}
+
+const actualCustomerRules = await db.insert(schema.customer_rules).values(mockCustomerRules).returning()
+
 const mockFineTunes: (typeof schema.fine_tunes.$inferInsert) []= [];
 for (let i = 0; i <1000; i++) {
 
     mockFineTunes.push({
-        ruleId: faker.helpers.arrayElement(actualRules).id,
+        customerRuleId: faker.helpers.arrayElement(actualCustomerRules).id,
         date: faker.date.between({from: new Date('2000-01-01'), to: new Date('2025-12-31T23:59:59.999Z')}),
-        customerId: faker.helpers.arrayElement(actualCustomers).id,
-        
+
         fineTune: faker.helpers.fake( 'Rule: {{person.firstName}} must not exceed {{number.int}} limits.'),
        
-        finalised: faker.helpers.maybe(() => true, {probability: 0.5}),
+        finalised: true,
+        finalisedAnalystId: faker.helpers.arrayElement(actualAnalysts).id,
 
         analystId: faker.helpers.arrayElement(actualAnalysts).id,
+
         comment: faker.helpers.maybe(() => faker.lorem.sentence(), {probability: 0.2})
     })
 }
@@ -64,39 +77,39 @@ await db.insert(schema.fine_tunes).values(mockFineTunes)
 
 
 
-const shuffleRules = faker.helpers.shuffle([...actualRules])
-const mockGoldenRules: (typeof schema.golden_rules.$inferInsert) []=[]
-for (let i = 0; i <5; i++) {
+// const shuffleRules = faker.helpers.shuffle([...actualRules])
+// const mockGoldenRules: (typeof schema.golden_rules.$inferInsert) []=[]
+// for (let i = 0; i <5; i++) {
 
-    // let primaryCrit = faker.location.country()
-    const uniqueRule = shuffleRules.pop()
-    mockGoldenRules.push({
-        ruleId: uniqueRule?.id, 
+//     // let primaryCrit = faker.location.country()
+//     const uniqueRule = shuffleRules.pop()
+//     mockGoldenRules.push({
+//         ruleId: uniqueRule?.id, 
         
-        fineTune: faker.helpers.fake( 'Rule: {{person.firstName}} must not exceed {{number.int}} limits.'),
+//         fineTune: faker.helpers.fake( 'Rule: {{person.firstName}} must not exceed {{number.int}} limits.'),
 
-        analystId: faker.helpers.arrayElement(actualAnalysts).id,
-        comment: faker.helpers.maybe(() => faker.lorem.sentence(), {probability: 0.2})
+//         analystId: faker.helpers.arrayElement(actualAnalysts).id,
+//         comment: faker.helpers.maybe(() => faker.lorem.sentence(), {probability: 0.2})
 
-    })
-}
+//     })
+// }
 
 
 
-const mockDrafts: (typeof schema.drafts.$inferInsert) [] = []
-for (let i = 0; i <5; i++) {
+// const mockDrafts: (typeof schema.drafts.$inferInsert) [] = []
+// for (let i = 0; i <5; i++) {
     
-    // let primaryCrit = faker.location.country()
-    mockDrafts.push({
-        ruleId: faker.helpers.arrayElement(actualRules).id,
-        date: faker.date.anytime(),
-        customerId: faker.helpers.arrayElement(actualCustomers).id,
+//     // let primaryCrit = faker.location.country()
+//     mockDrafts.push({
+//         ruleId: faker.helpers.arrayElement(actualRules).id,
+//         date: faker.date.anytime(),
+//         customerId: faker.helpers.arrayElement(actualCustomers).id,
         
-        fineTune: faker.helpers.maybe(() => faker.helpers.fake( 'Rule: {{person.firstName}} must not exceed {{number.int}} limits.'), {probability: 0.5}),
+//         fineTune: faker.helpers.maybe(() => faker.helpers.fake( 'Rule: {{person.firstName}} must not exceed {{number.int}} limits.'), {probability: 0.5}),
 
-        analystId: faker.helpers.arrayElement(actualAnalysts).id,
-        comment: faker.helpers.maybe(() => faker.lorem.sentence(), {probability: 0.2})
-    })
-}
+//         analystId: faker.helpers.arrayElement(actualAnalysts).id,
+//         comment: faker.helpers.maybe(() => faker.lorem.sentence(), {probability: 0.2})
+//     })
+// }
 
 
