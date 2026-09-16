39d627522966a30a83b968895dff94da1014d452
MKM2626 <mkmiller06@icloud.com>
2026-09-16 12:01:38 +0800

    commented out loading to see if page would display

--- src/routes/(protected)/details/[customerRuleId]/+page.svelte
+++ src/routes/(protected)/details/[customerRuleId]/+page.svelte
@@ -1,57 +1,67 @@
 <script lang="ts">
-	import { getDetails, deleteRow, search } from "#lib/remote/registers.remote.js";
-    import { getSearchContext } from "#lib/context/search";
-    import { beforeNavigate, goto } from "$app/navigation";
+    import { deleteFineTune } from "#lib/remote/delete.remote";
+    import { getFineTuneDetails } from "#lib/remote/getFineTuneDetails.remote";
+    import { getCustomerRules } from "#lib/remote/getCustomerRules.remote";
+    import { getCRSearchContext } from "#lib/context/customerRuleSearch";
+    import { goto } from "$app/navigation";
     import { diffWords } from 'diff';
-    import { fade, fly, slide } from 'svelte/transition';
-    import { toast } from '#lib/components/toast.svelte.js'
+    import { fade, slide } from 'svelte/transition';
+
+    import { toast } from "#lib/components/toast.svelte.js";
     import { authClient } from '#lib/auth-client'
     import { getDeleteContext } from "#lib/context/deleteAndProceed"
+	import { handleError } from "#lib/errors/handleError";
 
     const session = authClient.useSession() 
 
     let { params } = $props();
-    let id =$derived(params.customerRuleId);
+    let id = $derived(params.customerRuleId);
 
-    const moveOut = 0;
-    const delay = 0;
-    const moveIn = 0;
+
+    let ftSearchInfo = $state({ customerRuleId: "", page: 1, pageSize: 50, descending: true })
+
+    $effect(() => {
+        ftSearchInfo.customerRuleId = id
+    })
+    // const moveOut = 0;
+    // const delay = 0;
+    // const moveIn = 0;
 
     const next = getDeleteContext()
-    const searchInfo = getSearchContext()
+    const searchInfo = getCRSearchContext()
 
-    let selectedFineTune = $derived(await getDetails(id))
+    // let selectedFineTune = $derived(await getFineTuneDetails(ftSearchInfo))
     
 
     let deleting = $state(false)
-    let expanded = $state(false);
+    // let expanded = $state(false);
 
-    function getDiff(before: string, after: string) {
-        const differences = diffWords(before, after);
+    // function getDiff(before: string, after: string) {
+    //     const differences = diffWords(before, after);
 
-        let beforeResult = []
-        let afterResult = []
+    //     let beforeResult = []
+    //     let afterResult = []
 
-        for (const part of differences) {
-            if (part.removed) {
-                beforeResult.push({ class: "text-red-500", text: part.value})
-            }
-            else if (part.added) {
-                afterResult.push({ class: "text-green-500", text: part.value})
-            }
-            else {
-                beforeResult.push({ class: "text-text", text: part.value})
-                afterResult.push({ type: "same", text: part.value})
-            }
-        }
+    //     for (const part of differences) {
+    //         if (part.removed) {
+    //             beforeResult.push({ class: "text-red-500", text: part.value})
+    //         }
+    //         else if (part.added) {
+    //             afterResult.push({ class: "text-green-500", text: part.value})
+    //         }
+    //         else {
+    //             beforeResult.push({ class: "text-text", text: part.value})
+    //             afterResult.push({ type: "same", text: part.value})
+    //         }
+    //     }
 
-        return { 
-            before: beforeResult,
-            after: afterResult
-        }
-    }
+    //     return { 
+    //         before: beforeResult,
+    //         after: afterResult
+    //     }
+    // }
 
-    let otherDiffs = $derived(getDiff(selectedFineTune.before, selectedFineTune.after))
+    // let otherDiffs = $derived(getDiff(selectedFineTune.before, selectedFineTune.after))
 
     async function del() {
         if (deleting) return 
@@ -60,23 +70,19 @@
         deleting = true
 
         try {
-            const deleted = await deleteRow(id).updates(
-                search(searchInfo).withOverride((results) => ({
+            await deleteFineTune(id).updates(
+                getCustomerRules(searchInfo).withOverride((results) => ({
                     ...results
                 }))
             );
 
-            if (deleted) {
-                goto(`/details/${next.id}`)
-                toast.send('Deleted')
-            } else {
-                toast.send('Failed to delete', 'error')
-            }
-      
+            goto(`/details/${next.id}`)
+            toast.success('Deleted')
         }
         catch (error) {
+            handleError(error)
             history.back()
-            toast.send("Something went wrong", "error")
+            
         }
        finally {
             deleting=false
@@ -124,25 +130,25 @@
         <span class="w-28 text-text-muted">
             Date:
         </span>
-        {#key id}
+        <!-- {#key id}
 
             <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}>
                 {selectedFineTune.date.toISOString()}
             </span>
 
             
-        {/key}
+        {/key} -->
     </div>
 
     <div class="flex gap-4">
         <span class="w-28 text-text-muted">
             Rule:
         </span>
-        {#key id}
+        <!-- {#key id}
             <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}>
                 {selectedFineTune.rule}
             </span>
-        {/key}
+        {/key} -->
             
     </div>
 
@@ -150,11 +156,11 @@
         <span class="w-28 text-text-muted">
             Customer:
         </span>
-        {#key id}
+        <!-- {#key id}
             <span class='' in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}>
                 {selectedFineTune.customer}
             </span>
-        {/key}
+        {/key} -->
 
         
     </div>
@@ -163,43 +169,43 @@
         <span class="w-28 text-text-muted">
             Technology:
         </span>
-        {#key id}
+        <!-- {#key id}
             <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}>
                 {selectedFineTune.technology}
             </span>
-        {/key}
+        {/key} -->
     </div>
 
     <div class="flex gap-4">
         <span class="w-28 text-text-muted">
             Analyst:
         </span>
-        {#key id}
+        <!-- {#key id}
             <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}>
                 {selectedFineTune.analyst}
             </span>
-        {/key}
+        {/key} -->
     </div>
 
     <div class="flex gap-4">
         <span class="w-28 text-text-muted">
             Finalised: 
         </span>
-        {#key id}
+        <!-- {#key id}
             <input type="checkbox" checked={selectedFineTune.finalised} disabled in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}} class="appearance-none h-4 w-4 rounded-xs shadow-sm border border-red-500 bg-red-500 flex items-center justify-center before:content-['✗'] before:text-white before:text-xs before:font-medium checked:bg-green-500 checked:border-green-500 checked:before:content-['✓']">
-        {/key}
+        {/key} -->
     </div>
 
     <div class="flex gap-4">
         <span class="w-28 text-text-muted">
             Global: 
         </span>
-        {#key id}
+        <!-- {#key id}
             <input type="checkbox" checked={selectedFineTune.global} disabled in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}} class="appearance-none h-4 w-4 rounded-xs shadow-sm border border-red-500 bg-red-500 flex items-center justify-center before:content-['✗'] before:text-white before:text-xs before:font-medium checked:bg-green-500 checked:border-green-500 checked:before:content-['✓']">
-        {/key}
+        {/key} -->
     </div>
         
-    {#if selectedFineTune.global}
+    <!-- {#if selectedFineTune.global}
         <div class="bg-bg-light">
             <button
                 type="button"
@@ -218,7 +224,6 @@
                 >
                     <div class="mt-2 bg-bg-light rounded-lg grid grid-cols-4 gap-2">
                         {#each selectedFineTune.globals as global}
-                            <!-- in futer click on to take to same rule of that customer -->
                             <button 
                                 class="wrap-break-words text-wrap py-2 px-2 text-center bg-bg border border-border hover:border-indigo-500 rounded-lg"
                                 onclick={()=>goto(`/details/${global.id}`)}
@@ -231,7 +236,7 @@
                 </div>
             {/if}
         </div>
-    {/if}
+    {/if} -->
 </div>
 
 
@@ -240,13 +245,13 @@
         Previous Fine Tune:
     </h3>
     <div class="bg-bg-light shadow-md rounded-lg p-4">
-        {#key id}
+        <!-- {#key id}
             {#each otherDiffs.before as diff}
                 <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}} class="whitespace-pre-wrap inline {selectedFineTune.before !== 'Initial Rule' ? diff.class : ''}">
                     {diff.text}
                 </span>
             {/each}
-        {/key}
+        {/key} -->
     </div>
 </div>
 
@@ -255,13 +260,13 @@
         Updated Fine Tune:
     </h3>
     <div class="bg-bg-light shadow-md rounded-lg p-4">
-        {#key id}
+        <!-- {#key id}
             {#each otherDiffs.after as diff}
                 <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}}  class="whitespace-pre-wrap inline {selectedFineTune.before !== 'Initial Rule' ? diff.class : ''}">
                     {diff.text}
                 </span>
             {/each}
-        {/key}
+        {/key} -->
     </div>
 </div>
 
@@ -270,10 +275,10 @@
         Comments:
     </h3>
     <div class="bg-bg-light shadow-md rounded-lg p-4 transition:fade">
-        {#key id}   
+        <!-- {#key id}   
             <span in:fade={{ duration: moveIn, delay: delay }} out:fade={{ duration: moveOut}} >
                 {selectedFineTune.comment ?? "No comment"}
             </span>
-        {/key}  
+        {/key}   -->
     </div>
 </div>
